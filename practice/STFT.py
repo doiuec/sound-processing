5fdf2db688d78b7d8afddaa8b33009f836c58413
side_realms <87800512+doiuec@users.noreply.github.com>
2022-06-17 14:38:18 +0900

    Add files via upload

--- practice/STFT.py
+++ practice/STFT.py
@@ -1,42 +1,29 @@
 import csv
 import numpy as np
-import matplotlib.pyplot as pl
-from scipy import ceil, complex64, float64, hamming, zeros
-from scipy.fftpack import fft# , ifft
-from scipy import ifft 
+import matplotlib.pyplot as plt
+from scipy import signal
 
 data = []
 with open("result.csv","r") as f:
     reader = csv.reader(f)
     for row in reader:
-        data.append(row[0])
-fftLen = 60 
-win = hamming(fftLen) 
-step = fftLen//4
-fdata = [float(s) for s in data]
+        data.append(row[1])
 
-def stft(x, win, step):
-    l = len(x)
-    N = len(win)
-    M = int(ceil(float(l-N+step)/step))
-    new_x = zeros(N+((M-1)*step), dtype = float64)
-    x = new_x[:l]
+arr = np.array(data)
 
-    X = zeros([M,N],dtype = complex64)
-    for m in range(M):
-        start = step * M
-        X[m, :] = fft(new_x[start : start + N] * win)
-    return X
+farr = np.asarray(arr, dtype=float)
 
-spectrogram = stft(fdata, win, step)
+print(type(farr[0]))
 
-fig = pl.figure()
-fig.add_subplot(311)
-pl.plot(fdata)
-pl.xlim([0, len(fdata)])
-pl.title("Input signal", fontsize = 20)
-fig.add_subplot(313)
-pl.imshow(abs(spectrogram[:, : fftLen // 2 + 1].T), aspect = "auto", origin = "lower")
-pl.title("Spectrogram", fontsize = 20)
-pl.show()
-print(spectrogram)
+print(farr)
+
+fs = 60e5
+
+f, t, Sxx = signal.spectrogram(farr, fs)
+
+plt.pcolormesh(t, f, 10*np.log(Sxx)) #intensityを修正
+plt.ylabel('Frequency [Hz]')
+plt.xlabel('Time [sec]')
+cbar = plt.colorbar() #カラーバー表示のため追加
+cbar.ax.set_ylabel("Intensity [dB]") #カラーバーの名称表示のため追加
+plt.show()
